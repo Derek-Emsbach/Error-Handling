c7ba8694b71c1b4b03092984c79935af7ec684ca
Derek Emsbach <demsbach92@gmail.com>
2022-04-13 20:39:28 -0600

    Complete

--- error-handling.js
+++ error-handling.js
@@ -1,26 +1,52 @@
 // 1.
-function sum(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-  }
-  return sum;
-}
+// function sum(array) {
+//   if(typeof array !== 'array') {
+//     throw new Error('This is not an array!')
+//   } else {
+//     let sum = 0;
+//     for (let i = 0; i < array.length; i++) {
+//       sum += array[i];
+//     }
+//     return sum;
+//   }
+// }
 
-let res = sum(null);
-console.log(res);
+// let res = sum(null);
+// console.log(res);
+
+// try {
+//   console.log(res)
+// } catch(error) {
+//   console.error(error.name + ':' + error.message)
+// }
 
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
-// Your code here
+// sayName("Alex");
+// sayName(1);
+// // Your code here
+// function sayName(name) {
+//   if(typeof name !== 'string') {
+//     throw new Error('This is not a valid name, it is a number!')
+//   } else {
+//     return name
+//   }
+// }
+
+// try {
+//   sayName("Alex");
+//   sayName(1);
+// } catch(error) {
+//   console.error(error.name + ':' + error.message)
+// }
 
 // 3.
-function greet(greeting) {
-  if (!greeting) {
-    throw new Error("There was no greeting given.");
-  }
+// function greet(greeting) {
+//   if (!greeting) {
+//     throw new Error("There was no greeting given.");
+//   }
 
-  console.log(greeting);
-}
+//   console.log(greeting);
+// }
+
+// greet()
